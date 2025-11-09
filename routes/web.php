b390f2e74ea20be58de57ff30732c86a91e9aead
kamal10851 <kamal1085@gmail.com>
2025-11-09 16:38:48 +0530

    testing 04 - add route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/vite-check', function () {
+    $path = public_path('build/manifest.json');
+    return file_exists($path) ? '✅ Manifest found' : '❌ Manifest missing';
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
